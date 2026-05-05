e78e0b896c7ea5ee414aaf710310b4dd9aa40140
Noe Alberto Garcia Calvario <noealbertogarciacalvario@gmail.com>
2026-05-05 01:29:46 -0600

    Add full CI pipeline with API and UI tests

--- conftest.py
+++ conftest.py
@@ -7,6 +7,9 @@ from api.client import APIClient
 @pytest.fixture
 def driver():
     options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
 
     driver = webdriver.Chrome(options=options)
